b5860ddddddaefe6fb4192e24bcd035f827dddb2
Jaikciylegars95 <jackyheriniaina02@gmail.com>
2026-01-16 21:08:47 +0300

    debut

---
+++ components/hero.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+export default function Hero(){
+    return(
+        <section className="relative w-full min-h-screen flex items-center bg-[#f7f7f7] overflow-hidden">
+
+            <div className="container mx-auto px-6 lg:px-12"></div>
+
+            <div className="space-y-8"></div>
+        </section>
+
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**@type {import('tailwindcss').config} */
+module.exports = {
+    content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+],
+theme: {
+    extends:{}
+},
+plugins: [],
+
+};
